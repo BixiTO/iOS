83e70f9848df9df4e6d332423cfa6238c8475473
Andrew Laffin <andrewlaffin@gmail.com>
2013-03-16 02:18:28 -0400

    api key

---
+++ BixiTO/Private.h
@@ -0,0 +1,14 @@
+//
+//  Private.h
+//  BixiTO
+//
+//  Created by Andrew Laffin on 2013-03-16.
+//  Copyright (c) 2013 BixiTO. All rights reserved.
+//
+
+#ifndef BixiTO_Private_h
+#define BixiTO_Private_h
+
+#define GOOGLEMAPSAPIKEY @"AIzaSyCXytEtX3sfi0efW-ew45mmGm6r_eQHxBw"
+
+#endif
